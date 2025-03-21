f5d88157c2ebd82b386262696076b3e0916f0242
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:21:02 +0930

    Update batch_repair_geometries.py

--- batch_repair_geometries.py
+++ batch_repair_geometries.py
@@ -1,7 +1,7 @@
 import os
 
-folder_path = 'C:\\Users\\qw2\\Desktop\\Fire_Scars_Archive\\shapefiles'
-save_path = 'C:\\Users\\qw2\\Desktop\\Fire_Scars_Archive\\geopackages'
+folder_path = 'Fire_Scars_Archive\\shapefiles'
+save_path = 'Fire_Scars_Archive\\geopackages'
 
 for file in os.scandir(folder_path):
     if file.name.split('.')[-1] == 'shp':
